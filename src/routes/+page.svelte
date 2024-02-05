59dabdeae4f1bbfff52bb4deb275576b04057f26
reiji1020 <sk@reiji1020.info>
2024-02-06 02:58:54 +0900

    [update]using ssckit4svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import { Header } from 'cclkit4svelte'
+</script>
+
+<Header bgColor="--strawberry-pink" height="--hd-nomal"/>
+
+<style>
+    :root {
+        background: #fff;
+    }
+</style>
+
