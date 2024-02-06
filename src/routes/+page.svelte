ce85a479e1c9a739d79863241fc04613fe8ff319
reiji1020 <sk@reiji1020.info>
2024-02-07 03:33:35 +0900

    [update]add footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,29 @@
 <script>
-    import { Header } from 'cclkit4svelte'
+    import { Header } from 'cclkit4svelte';
+    import { Footer } from "cclkit4svelte";
 </script>
 
-<Header bgColor="--strawberry-pink" height="--hd-nomal"/>
+<div class="ccl-header">
+    <a href="/">
+        <Header bgColor="--strawberry-pink" height="--hd-nomal"/>
+    </a>
+</div>
+<main>
+    hogehoge
+</main>
+<div class="ccl-footer">
+    <Footer class="ccl-footer" bgColor="--strawberry-pink" />
+</div>
 
-<style>
-    :root {
-        background: #fff;
-    }
+<style lang="scss">
+  .ccl-header{
+    position: sticky;
+    bottom: 100vh;
+  }
+
+  .ccl-footer{
+    position: sticky;
+    top: 100vh;
+  }
 </style>
 
