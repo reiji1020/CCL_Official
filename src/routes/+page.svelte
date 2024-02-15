546556e32073c4d3cf6d02a85da819fbd4149f6b
reiji1020 <sk@reiji1020.info>
2024-02-16 03:29:00 +0900

    [update]トピック掲出用のカードを追加

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,5 @@
 <script>
-    import { Header } from 'cclkit4svelte';
-    import { Footer } from "cclkit4svelte";
+    import { Header, Footer, Card } from 'cclkit4svelte';
     import SubTitle from "$lib/Subtitle.svelte";
     import ActivityLog from "$lib/ActivityLog.svelte";
     import Member from "$lib/Member.svelte";
@@ -15,7 +14,11 @@
 <main>
     <!--    MainCard-->
     <SubTitle />
-    <!--    ActivityLog-->
+    <div class="activityCardWrapper">
+        <Card borderColor="--pineapple-yellow" bgColor="--lemon-yellow" src="./thumbnail.png" altText="CANDYCHUPSLab." title="技術書典16にサークル参加を申し込みました！" cardText="2024年春に開催される技術書典16へサークル出典を申し込みました。Svelte関連の本が出ます。" />
+        <Card borderColor="--strawberry-pink" bgColor="--peach-pink" src="./20240201.jpg" altText="実践Reactコンポーネントライブラリ開発" title="新刊が発売されました！" cardText="インプレスR&D様よりReactコンポーネントライブラリの本を出させていただきました。" />
+        <Card borderColor="--strawberry-pink" bgColor="--peach-pink" src="./20221021.jpg" altText="実践入門ビジュアルプログラミング言語" title="vvvv&vvvv gamma本発売中！" cardText="各ネット書店でビジュアルプログラミング言語vvvvとvvvv gammaの本を発売中です。" />
+    </div>
     <ActivityLog />
     <!--    Member-->
     <Member />
@@ -29,6 +32,12 @@
   .ccl-header{
     position: sticky;
     bottom: 100vh;
+  }
+
+  .activityCardWrapper {
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
   }
 
   .ccl-footer{
