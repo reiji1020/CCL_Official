085e1236aa1ca49f0bcad6dc2c69c1b544caba4a
reiji1020 <sk@reiji1020.info>
2024-04-06 14:46:23 +0900

    [update]パッケージ更新＆モバイルのスタイル崩れを修正

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-    import { Table, Card, Footer, Header} from 'cclkit4svelte';
+    import { Table, Card, Footer, Header, CCLVividColor, HeaderHeight } from 'cclkit4svelte';
     import SubTitle from "$lib/logo/Subtitle.svelte";
     import ActivityLog from "$lib/logo/ActivityLog.svelte";
     import Member from "$lib/logo/Member.svelte";
@@ -13,7 +13,7 @@
 <div class="ccl-header">
     <!--    HEADER      -->
     <a href="/">
-        <Header bgColor="--strawberry-pink" height="--hd-nomal"/>
+        <Header bgColor={CCLVividColor.STRAWBERRY_PINK} height={HeaderHeight.NOMAL}/>
     </a>
 </div>
 <main>
@@ -22,21 +22,21 @@
     <div class="activity-card-wrapper">
         <div class="card-padding">
             <a href="https://github.com/reiji1020/ccl-component-kit4svelte" target="_blank">
-                <Card borderColor="--soda-blue" src="./thumbnail.png"
+                <Card borderColor={CCLVividColor.SODA_BLUE} src="./thumbnail.png"
                       altText="Svelteコンポーネントライブラリ" title="UIライブラリ開発中！"
                       cardText="完全Svelte製のUIコンポーネントライブラリ「cclkit4svelte」開発中です！"/>
             </a>
         </div>
         <div class="card-padding">
             <a href="https://reiji1020.hatenablog.com/entry/2024/03/15/210000" target="_blank">
-                <Card borderColor="--pineapple-yellow" src="./thumbnail.png"
+                <Card borderColor={CCLVividColor.PINEAPPLE_YELLOW} src="./thumbnail.png"
                       altText="CANDYCHUPSLab." title="技術書典16のオン・オフ参加決定！"
                       cardText="技術書典16のスペースを無事にいただくことができました！新刊情報は随時公開します。"/>
             </a>
         </div>
         <div class="card-padding">
             <a href="https://nextpublishing.jp/book/13898.html" target="_blank">
-                <Card borderColor="--strawberry-pink" src="./20240201.jpg"
+                <Card borderColor={CCLVividColor.STRAWBERRY_PINK} src="./20240201.jpg"
                       altText="実践Reactコンポーネントライブラリ開発" title="新刊が発売されました！"
                       cardText="インプレスR&D様よりReactコンポーネントライブラリの本を出させていただきました。"/>
             </a>
@@ -44,7 +44,7 @@
     </div>
     <ActivityLog/>
     <div style="display: flex; justify-content: center; align-items: center">
-        <Table tableColor="--pineapple-yellow" dataHeader={updateTitle} tableData={updateData} />
+        <Table tableColor={CCLVividColor.PINEAPPLE_YELLOW} dataHeader={updateTitle} tableData={updateData} />
     </div>
     <!--    Member-->
     <Member/>
@@ -53,7 +53,7 @@
 <div class="ccl-footer">
     <!--    FOOTER      -->
     <NavBar />
-    <Footer bgColor="--strawberry-pink"/>
+    <Footer bgColor={CCLVividColor.STRAWBERRY_PINK}/>
 </div>
 
 <style lang="scss">
